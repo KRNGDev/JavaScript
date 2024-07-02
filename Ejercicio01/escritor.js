cac211dc35e2b74f0515151b275eda3ecdad8355
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-07-02 16:09:21 +0200

    sum

---
+++ Ejercicio01/escritor.js
@@ -0,0 +1,12 @@
+function dibujar(parametro, texto) {
+  document.getElementById(parametro).innerHTML = texto;
+}
+
+console.log("Hola holli");
+document.getElementById("p1").innerHTML = "Juancarlos mola";
+escribir();
+
+
+function escribir() {
+  document.getElementById("h2").innerHTML = "Patata";
+}
---
+++ Ejercicio02/funciones.js
@@ -0,0 +1,5 @@
+document.getElementById('cuerpo').innerHTML= 'Este es el cuerpo Moreno';
+function Footer(){
+    document.getElementById('pie').innerHTML='Este es el footer';
+
+}
