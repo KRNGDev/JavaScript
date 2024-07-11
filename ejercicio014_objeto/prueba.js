57058988dd5c7e40e76efd3a62637bd08ff6ae0e
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-07-11 20:44:58 +0200

    sum

---
+++ ejercicio014_objeto/prueba.js
@@ -0,0 +1,23 @@
+let estadosUnidos={
+    nombre: "Estados Unido",
+    continente: "America"
+}
+let harrisonFord={
+    nombre:"Harrison Ford",
+    pais: estadosUnidos
+}
+let markHamil={
+    nombre:"Mark Hamil",
+    pais:estadosUnidos
+}
+
+let starWarsEpisodioIV={
+    titulo:"Star Wars EpisodioIV",
+    director: "George Luca",
+    actores:[harrisonFord,markHamil]
+}
+let starWarsEpisodioV={
+    actores:[harrisonFord,markHamil]
+}
+
+letsagaStarwars=[starWarsEpisodioIV,starWarsEpisodioV];
---
+++ ejercicio015_juegos/app.js
@@ -0,0 +1,62 @@
+let juegos = [];
+
+
+
+
+
+//Recuopera ELJSON guardado
+let juegosGuardados= localStorage.getItem("juegos");
+if(juegosGuardados!=null){
+    juegos= JSON.parse(juegosGuardados);
+    
+}
+//agregamos un listener al boton
+document.querySelector("#bAgregar").addEventListener("click", (event) => {
+  let nombre = document.querySelector("#iNombre").value;
+  let genero = document.querySelector("#iGenero").value;
+  let clasificacion = document.querySelector("#iClasificacion").value;
+  let imagen = document.querySelector("#iIMG").value;
+
+  //creamos el objeto
+  let juego = {
+    nombre: nombre,
+    genero: genero,
+    clasificacion: clasificacion,
+    imagen: imagen,
+  };
+
+  juegos.push(juego);
+  generarListaJuegos();
+});
+
+function generarListaJuegos() {
+  let cardJuego = "";
+
+  juegos.map((juego) => {
+    cardJuego += ` <div class="card">
+            <div class="datos">
+            <div class="nombre">${juego.nombre}</div>
+            <div class="genero">${juego.genero}</div>
+            <div class="clasificacion">${juego.clasificacion}</div>
+            </div>
+            <div class="imagen">
+            <img src="${juego.imagen}">
+            </div>
+        </div>`;
+  });
+
+  document.querySelector("#contenido").innerHTML = cardJuego;
+
+  document.querySelector("#iNombre").value ="";
+  document.querySelector("#iGenero").value ="";
+  document.querySelector("#iClasificacion").value ="";
+  document.querySelector("#iIMG").value ="";
+}
+
+document.querySelector("#guardar").addEventListener("click",guardar);
+
+function guardar(){
+    let strJuegos = JSON.stringify(juegos);
+    localStorage.setItem("juegos", strJuegos);
+
+}
