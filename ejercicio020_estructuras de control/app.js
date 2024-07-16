e6cc63589e6fc638924e0fb08b786b8cb72baa8c
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-07-16 16:04:18 +0200

    sum

--- ejercicio020_estructuras de control/app.js
+++ ejercicio020_estructuras de control/app.js
@@ -1,11 +1,11 @@
 let edad=50;
-let estadoCivil= "Soltero";
+let estadoCivi= "Soltero";
 let idiomas =["Ingles","Español","Frances","Chino"];
 let permisoConducir=true;
 let movillidadGeografica=true;
 
 
-if (edad>18&estadoCivil=="Soltero"&idiomas.includes("Ingles")==true&permisoConducir&movillidadGeografica){
+if (edad>18&estadoCivi=="Soltero"&idiomas.includes("Ingles")==true&permisoConducir&movillidadGeografica){
     console.log("ta to bien");
 
 }
@@ -13,3 +13,14 @@ else{
     console.log("no ta bien");
 
 }
+
+//Variante mas molona
+let mayorEdad= edad>18;
+let esSoltero=estadoCivi=="Soltero";
+let sabeIngles=idiomas.includes("Ingles");
+if( mayorEdad && esSoltero && sabeIngles && permisoConducir && movillidadGeografica){
+    console.log("asi tmb ta to bien");
+}else{
+    console.log("asi tmp ta to bien");
+}
+
