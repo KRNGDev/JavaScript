f62da40450f31a3a6e011bb33cdb7c3441f993fc
Alberto Lopez Martinez <krng.taure@gmail.com>
2024-07-16 17:03:36 +0200

    sum

---
+++ ejercicio021_superejercicio/clliente-http-xmlhttpreques.js
@@ -0,0 +1,17 @@
+function getVideojuegos(url,port,recurso){
+    const client = new XMLHttpRequest();
+
+    client.addEventListener("readystatechange",()=>{
+        const isDone = client.readyState===4;
+        const isOk= client.status ===200;
+
+        if(isDone&& isOk){
+            console.log(client.responseText );
+        }
+    });
+
+    client.open("GET",`${url}:${port}/${recurso}`);
+    client.send();
+}
+
+getVideojuegos("http://localhost",5500,"/ejercicio021_superejercicio/datos.json");
